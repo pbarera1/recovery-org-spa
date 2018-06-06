36b50c61df6a7f56929f16b64f69fb48b1aeff44
matt-mcdaniel <matt.h.mcdaniel@gmail.com>
2018-06-06 13:00:57 -0700

    first commit

---
+++ .next/bundles/pages/index.js
@@ -0,0 +1,1 @@
+module.exports=__NEXT_REGISTER_PAGE("/",function(){var e=webpackJsonp([2],{191:function(e,t,n){e.exports=n(192)},192:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:true});var r=n(13);var u=n.n(r);t["default"]=function(){return u.a.createElement("div",null,"Next JS!")}}},[191]);return{page:e.default}});
