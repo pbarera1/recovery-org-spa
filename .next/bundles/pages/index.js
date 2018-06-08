d6aa3ee2c57d18f342170c2f7ff86466914b79e8
matt-mcdaniel <matt.h.mcdaniel@gmail.com>
2018-06-07 17:31:15 -0700

    remove build folder

--- .next/bundles/pages/index.js
+++
@@ -1,1 +0,0 @@
-module.exports=__NEXT_REGISTER_PAGE("/",function(){var e=webpackJsonp([2],{191:function(e,t,n){e.exports=n(192)},192:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:true});var u=n(13);var r=n.n(u);t["default"]=function(){return r.a.createElement("div",null,"Deployment Success!")}}},[191]);return{page:e.default}});
