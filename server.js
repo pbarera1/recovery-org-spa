1dc3f0b20692ec50b7655cee5741a4076f6689d6
matt-mcdaniel <matt.h.mcdaniel@gmail.com>
2018-06-12 12:52:53 -0700

    get dynamic port

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = next({
     dev
 });
 const handle = app.getRequestHandler();
+const PORT = process.env.PORT || 3000;
 
 app.prepare()
     .then(() => {
@@ -23,7 +24,7 @@ app.prepare()
             return handle(req, res);
         });
 
-        server.listen(3000, err => {
+        server.listen(PORT, err => {
             if (err) throw err;
             console.log('> Ready on http://localhost:3000');
         });
