22c1856f0daca5c56ae9c0fbc3f21195bbedb99d
matt-mcdaniel <matt.h.mcdaniel@gmail.com>
2018-06-13 16:38:34 -0700

    add default value for getProp

--- pages/state.js
+++ pages/state.js
@@ -5,7 +5,7 @@ import withInitialProps from '../lib/withInitialProps';
 
 class State extends Component {
     formatAdvancedCustomFields = () => {
-        const acfObject = getProp(this.props.data, 'acf');
+        const acfObject = getProp(this.props.data, 'acf', {});
 
         return Object.keys(acfObject).reduce((acc, cur) => {
             let text = acfObject[cur];
